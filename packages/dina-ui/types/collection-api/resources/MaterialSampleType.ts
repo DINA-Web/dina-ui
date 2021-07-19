5eaa65d3bd0973f0803b7d0fb77d00a97d04c75d
Shemy <shemy.gan@gmail.com>
2021-07-19 16:00:25 -0400

    23276 - Add material sample type options to type file

--- packages/dina-ui/types/collection-api/resources/MaterialSampleType.ts
+++ packages/dina-ui/types/collection-api/resources/MaterialSampleType.ts
@@ -1,4 +1,5 @@
 import { KitsuResource } from "kitsu";
+import { DINAUI_MESSAGES_ENGLISH } from "packages/dina-ui/intl/dina-ui-en";
 
 export interface MaterialSampleTypeAttributes {
   type: "material-sample-type";
@@ -9,3 +10,36 @@ export interface MaterialSampleTypeAttributes {
 }
 
 export type MaterialSampleType = KitsuResource & MaterialSampleTypeAttributes;
+
+export type MaterialSampleTypeValues =
+  | "Whole Organism"
+  | "Partial Organism"
+  | "Mixed Specimen"
+  | "Molecular Sample"
+  | "Geological";
+
+export const MATERIAL_SAMPLE_TYPE_OPTIONS: {
+  labelKey: keyof typeof DINAUI_MESSAGES_ENGLISH;
+  value: MaterialSampleTypeValues;
+}[] = [
+  {
+    labelKey: "field_materialSampleType_wholeOrganism_label",
+    value: "Whole Organism"
+  },
+  {
+    labelKey: "field_materialSampleType_partialOrganism_label",
+    value: "Partial Organism"
+  },
+  {
+    labelKey: "field_materialSampleType_mixedSpecimen_label",
+    value: "Mixed Specimen"
+  },
+  {
+    labelKey: "field_materialSampleType_molecularSample_label",
+    value: "Molecular Sample"
+  },
+  {
+    labelKey: "field_materialSampleType_geological_label",
+    value: "Geological"
+  }
+];
